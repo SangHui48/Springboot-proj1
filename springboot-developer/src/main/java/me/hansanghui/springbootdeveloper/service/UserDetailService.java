415406dc4882773f1f41b4784a4c399421d908ae
SangHui48 <vodlrm123@naver.com>
2024-06-02 19:02:59 +0900

    6/2 210p

---
+++ springboot-developer/src/main/java/me/hansanghui/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,22 @@
+package me.hansanghui.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hansanghui.springbootdeveloper.domain.User;
+import me.hansanghui.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 사용자 이름(email)으로 사용
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
